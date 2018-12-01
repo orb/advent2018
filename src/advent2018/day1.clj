f63f6e6be7b92df334733048bbec4f13f2fe9bb9
Norman Richards <orb@nostacktrace.com>
2018-11-30 23:36:48 -0600

    day 1

--- src/advent2018/day1.clj
+++ src/advent2018/day1.clj
@@ -1,2 +1,24 @@
-(ns advent2018.day1)
+(ns advent2018.day1
+  (:require [clojure.string :as string]))
+
+(defn read-numbers [text]
+  (->> (string/split text #"\n")
+       (map string/trim)
+       (map #(Integer/parseInt %) )))
+
+(defn part1 [text]
+  (let [numbers (read-numbers text)]
+    (reduce + numbers)))
+
+(defn part2 [text]
+  (loop [numbers (cycle (read-numbers text))
+         seen #{0}
+         frequency  0]
+    (let [number (first numbers)
+          new-frequency (+ frequency number)]
+      (if (seen new-frequency)
+        new-frequency
+        (recur (rest numbers)
+               (conj seen new-frequency)
+               new-frequency)))))
 
--- test/advent2018/day1_test.clj
+++ test/advent2018/day1_test.clj
@@ -1,7 +1,21 @@
 (ns advent2018.day1-test
   (:require [advent2018.day1 :refer :all]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all]
+            [clojure.java.io :as io]))
+
+(def my-input (slurp (io/resource "day1-input.txt")))
+
+(deftest test-read
+  (is (= [-1 1 2] (read-numbers " -1  \n1\n+2   "))))
 
 (deftest test-part1
-  (is (= 1 0)))
+  (is (= 3 (part1 "+1\n +1\n +1")))
+  (is (= 553 (part1 my-input))))
+
+(deftest test-part2
+  (is (= 0 (part2 "+1\n -1")))
+  (is (= 10 (part2 "+3\n +3\n +4\n -2\n -4")))
+  (is (= 5 (part2 "-6\n +3\n +8\n +5\n -6")))
+  (is (= 14 (part2 "+7\n +7\n -2\n -7\n -4")))
+  (is (= 78724 (part2 my-input))))
 
