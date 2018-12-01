e44cbf017fbb401a43a90b169dec4140703ac124
Norman Richards <orb@nostacktrace.com>
2018-12-01 09:23:01 -0600

    refactor day1

--- src/advent2018/day1.clj
+++ src/advent2018/day1.clj
@@ -4,21 +4,30 @@
 (defn read-numbers [text]
   (->> (string/split text #"\n")
        (map string/trim)
-       (map #(Integer/parseInt %) )))
+       (map #(Integer/parseInt %))))
 
 (defn part1 [text]
   (let [numbers (read-numbers text)]
     (reduce + numbers)))
 
+(defn first-dup
+  "returns the first duplicate number in a seq of numbers, if any"
+  [numbers]
+  (loop [[number & rest-numbers] numbers
+         seen #{}]
+    (when number
+      (if (seen number)
+        number
+        (recur rest-numbers
+               (conj seen number))))))
+
+(defn sum-stream
+  "returns a stream of the partial sum of a seq of numbers.
+   assumption that the initial sum, 0, should be included"
+  [numbers]
+  (reductions + 0 numbers))
+
 (defn part2 [text]
-  (loop [numbers (cycle (read-numbers text))
-         seen #{0}
-         frequency  0]
-    (let [number (first numbers)
-          new-frequency (+ frequency number)]
-      (if (seen new-frequency)
-        new-frequency
-        (recur (rest numbers)
-               (conj seen new-frequency)
-               new-frequency)))))
+  (let [numbers (cycle (read-numbers text))]
+    (first-dup (sum-stream numbers))))
 
--- test/advent2018/day1_test.clj
+++ test/advent2018/day1_test.clj
@@ -12,10 +12,25 @@
   (is (= 3 (part1 "+1\n +1\n +1")))
   (is (= 553 (part1 my-input))))
 
+
+(deftest test-sums
+  (is (= [0 1 3 6]
+         (sum-stream [1 2 3])))
+  (is (= (range 10)
+         (take 10 (sum-stream (repeatedly (constantly 1))))))
+  (is (= (take 7 (cycle [0 5]))
+         (take 7 (sum-stream (cycle [5 -5]))))))
+
+(deftest test-dup
+  (is (nil? (first-dup [8 6 7 5 3 0 9])))
+  (is (= 4 (first-dup [2 4 6 8 10 5 1 4]))))
+
 (deftest test-part2
   (is (= 0 (part2 "+1\n -1")))
   (is (= 10 (part2 "+3\n +3\n +4\n -2\n -4")))
   (is (= 5 (part2 "-6\n +3\n +8\n +5\n -6")))
   (is (= 14 (part2 "+7\n +7\n -2\n -7\n -4")))
   (is (= 78724 (part2 my-input))))
+
+
 
